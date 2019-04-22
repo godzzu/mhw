326762ba0b699ff806b117e471bc26d729ff614b
godzzu <bedevilu@hanmail.net>
2019-04-22 17:14:51 +0900

    1

--- js/common.js
+++ js/common.js
@@ -2,3 +2,9 @@
 	function menu_set(){
 		$("#nav").load('../monster/left_menu.html');
 	}
+
+	function moster_info(path){
+		alert(path);
+		$("#body").children().remove();
+		$("#body").load('../monster/'+path+'.html');
+	}
